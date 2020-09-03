5d7626640818e44ef00ea6ef6fb261c9fab43544
Ray Lothian <ray.lothian@yahoo.com>
2020-09-03 07:58:11 +0200

    possible fix for #6

--- Scripts/Popup.js
+++ Scripts/Popup.js
@@ -1,221 +1,227 @@
-    var main = document.getElementsByTagName('main')[0];
-    var resetPreset = document.getElementById('resetPreset');
-    var svgNS = "http://www.w3.org/2000/svg";
+/* global loadPresets, browser, Sortable, resizeWindow, comparePresets */
 
-    var presetsClass;
-    var data;
-    var shortcuts;
+const main = document.getElementsByTagName('main')[0];
+const resetPreset = document.getElementById('resetPreset');
 
+let data;
+let shortcuts;
+let workArea;
 
-    document.addEventListener('DOMContentLoaded', () => {
-      loadPresets(Initialize)
-    });
+window.addEventListener('load', () => chrome.system.display.getInfo({}, info => {
+  chrome.windows.getCurrent(win => {
+    const o = info.filter(o => {
+      return win.left >= o.workArea.left &&
+        win.top >= o.workArea.top &&
+        win.width <= o.workArea.width &&
+        win.height <= o.workArea.height;
+    }).shift() || info[0];
+    workArea = o.workArea;
+    loadPresets(initialize);
+  });
+}));
 
-    function Initialize(loadedData) {
-      data = loadedData
-        
-      function initWithBrowserCommands(browserCommands) {
-        shortcuts = browserCommands.filter(cmd => cmd.name.match(/resize\d/))
+function initialize(loadedData) {
+  data = loadedData;
 
-        for (var i = 0; i < data.length; i++) {
-          CreatePreset(data[i], i);
+  function initWithBrowserCommands(browserCommands) {
+    shortcuts = browserCommands.filter(cmd => cmd.name.match(/resize\d/));
+
+    for (let i = 0; i < data.length; i++) {
+      createPreset(data[i], i);
+    }
+
+    Sortable.create(main, {
+      animation: 250,
+      onStart: function() {
+        const presets = document.getElementsByClassName('preset');
+        for (let i = 0; i < presets.length; i++) {
+          presets[i].classList.add('presetDisableHover');
         }
+      },
+      onEnd: function() {
+        const presets = document.getElementsByClassName('preset');
+        for (let i = 0; i < presets.length; i++) {
+          presets[i].classList.remove('presetDisableHover');
+          presets[i].setAttribute('data-sortorder', i);
+          data.find(p => p.id === presets[i].getAttribute('data-id')).sortorder = i;
+        }
+        chrome.storage.local.set({
+          'presets': data
+        }, () => {
 
-        Sortable.create(main, {
-          animation: 250,
-          onStart: function(evt) {
-            var presets = document.getElementsByClassName('preset');
-            for (var i = 0; i < presets.length; i++) {
-              presets[i].classList.add('presetDisableHover');
-            }
-          },
-          onEnd: function() {
-            var presets = document.getElementsByClassName('preset');
-            for (var i = 0; i < presets.length; i++) {
-              presets[i].classList.remove('presetDisableHover');
-              presets[i].setAttribute('data-sortorder',i);
-              data.find(p=>p.id==presets[i].getAttribute('data-id')).sortorder=i;
-              }
-            chrome.storage.local.set({
-              "presets": data
-            }, () => {
-
-            });
-          }
         });
       }
-
-      try {
-        browser.commands.getAll().then(initWithBrowserCommands)
-      } catch (err) {
-        chrome.commands.getAll((initWithBrowserCommands))
-      }
-    }
-
-    function CreatePreset(preset, index) {
-      if(preset.isempty==false)
-      {
-        var shortcut = index < shortcuts.length ? shortcuts[index] : null;
-
-        var presetElem = document.createElement('div');
-        presetElem.className = 'preset';
-        presetElem.setAttribute('data-sortorder',preset.sortorder);
-        presetElem.setAttribute('data-id', preset.id);
-        presetElem.setAttribute('data-width', preset.width);
-        presetElem.setAttribute('data-height', preset.height);
-        presetElem.setAttribute('data-left', preset.left);
-        presetElem.setAttribute('data-top', preset.top);
-        var btnRemove = document.createElement('i');
-        btnRemove.className = "icon-trash";
-        btnRemove.setAttribute('data-removeElem', preset.id);
-        var sizeElem = document.createElement('span');
-        sizeElem.textContent = Math.round(preset.width / 100 * screen.width) + ' * ' +
-                             Math.round(preset.height/100 * screen.height);
-        var img = document.createElement('div');
-        var canvas = document.createElement('canvas');
-        canvas.setAttribute('id', 'c' + preset.id);
-        canvas.setAttribute('width', '64');
-        canvas.setAttribute('height', '64');
-        presetElem.appendChild(btnRemove);
-        presetElem.appendChild(sizeElem);
-        presetElem.appendChild(img);
-        presetElem.appendChild(canvas);
-        if(shortcut != null) {
-          var key = shortcut.shortcut.replace('MacCtrl', 'Ctrl')
-          var hotkeyInfo = document.createElement('span');
-          hotkeyInfo.className = 'hotkey';
-          hotkeyInfo.textContent = key
-          presetElem.appendChild(hotkeyInfo)
-        }
-        main.appendChild(presetElem);
-        DrawRec(canvas.id, Math.round(preset.left /100 * 45.6), Math.round(preset.top / 100 * 30), Math.round(preset.width / 100 *  45.6), Math.round(preset.height/100  * 30));
-      }
-      else
-      {
-        var presetElem = document.createElement('div');
-        presetElem.className = 'preset';
-        presetElem.setAttribute('data-sortorder',preset.sortorder);
-        presetElem.setAttribute('data-id', preset.id);
-        main.appendChild(presetElem);
-      }
-    }
-
-    function DrawRec(canvasId, x, y, w, h) {
-      var canvas = document.getElementById(canvasId);
-      var ctx = canvas.getContext("2d");
-      ctx.fillStyle = "#663399";
-      ctx.rect(x + 9, y + 15, w, h);
-      ctx.fill();
-    }
-
-
-    var selectedElem;
-    document.addEventListener('click', (e) => {
-      let dialog = document.getElementsByClassName("dialogWrapper")[0];
-      let dialogReset = document.getElementsByClassName("dialogWrapper")[1];
-
-      if (e.target.className == "icon-trash") {
-        dialog.style.display = "flex";
-        selectedElem = e.target.parentElement.getAttribute('data-id');
-      }
-      switch (e.target.id) {
-        case "btnEdit":
-          var presets = document.getElementsByClassName('preset');
-          if (e.target.state == undefined) {
-            for (var i = 0; i < presets.length; i++) {
-              presets[i].classList.add('editMode');
-            }
-            e.target.classList.add('editMode-cancel');
-            e.target.state = "IsInEditMode";
-          } else {
-            for (var i = 0; i < presets.length; i++) {
-              presets[i].classList.remove('editMode');
-            }
-            e.target.classList.remove('editMode-cancel');
-            e.target.state = undefined;
-          }
-          break;
-        case "btnNewPreset":
-          chrome.tabs.create({
-            'url': '../CustomPreset.html'
-          });
-          break;
-        case "btnCancel":
-          dialog.style.display = "none";
-          break;
-          case "btnResetCancel":
-            dialogReset.style.display = "none";
-            break;
-        case "btnOk":
-          dialog.style.display = "none";
-          var dataID = selectedElem;
-          EmptyMain();
-          RemovePreset(dataID);
-          var presets = document.getElementsByClassName('preset');
-          for (var i = 0; i < presets.length; i++) {
-            presets[i].classList.remove('editMode');
-          }
-          var btnEdit = document.getElementById('btnEdit');
-          if (btnEdit.state != undefined) {
-            btnEdit.classList.remove('editMode-cancel');
-            btnEdit.state = undefined;
-          }
-            data.sort(comparePresets);
-          for (var i = 0; i < data.length; i++) {
-            CreatePreset(data[i], i);
-          }
-          break;
-          case "btnResetOk":
-          chrome.storage.local.remove('presets', () => chrome.runtime.sendMessage({
-            method: 'notify',
-            message: 'Layouts are reset to the default values'
-          }));
-
-          const request = new XMLHttpRequest();
-          request.open('GET', '../Content/Data.json');
-          request.onload = () => {
-            data = JSON.parse(request.responseText).presets;
-            EmptyMain();
-            Initialize();
-          };
-          request.send();
-            dialogReset.style.display = "none";
-          break;
-        default:
-      }
-
-      if (e.target.className == "preset") {
-        let x = parseInt(e.target.getAttribute('data-left'));
-        let y = parseInt(e.target.getAttribute('data-top'));
-        let w = parseInt(e.target.getAttribute('data-width'));
-        let h = parseInt(e.target.getAttribute('data-height'));
-        resizeWindow(x, y, w, h);
-
-      }
     });
+  }
 
+  try {
+    browser.commands.getAll().then(initWithBrowserCommands);
+  }
+  catch (err) {
+    chrome.commands.getAll((initWithBrowserCommands));
+  }
+}
 
+function createPreset(preset, index) {
+  if (preset.isempty === false) {
+    const shortcut = index < shortcuts.length ? shortcuts[index] : null;
 
-    function EmptyMain() {
-      while (main.firstChild) {
-        main.removeChild(main.firstChild);
-      }
+    const presetElem = document.createElement('div');
+    presetElem.className = 'preset';
+    presetElem.setAttribute('data-sortorder', preset.sortorder);
+    presetElem.setAttribute('data-id', preset.id);
+    presetElem.setAttribute('data-width', preset.width);
+    presetElem.setAttribute('data-height', preset.height);
+    presetElem.setAttribute('data-left', preset.left);
+    presetElem.setAttribute('data-top', preset.top);
+    const btnRemove = document.createElement('i');
+    btnRemove.className = 'icon-trash';
+    btnRemove.setAttribute('data-removeElem', preset.id);
+    const sizeElem = document.createElement('span');
+    sizeElem.textContent =
+      Math.round(preset.width / 100 * workArea.width) + ' * ' +
+      Math.round(preset.height / 100 * workArea.height);
+    const img = document.createElement('div');
+    const canvas = document.createElement('canvas');
+    canvas.setAttribute('id', 'c' + preset.id);
+    canvas.setAttribute('width', '64');
+    canvas.setAttribute('height', '64');
+    presetElem.appendChild(btnRemove);
+    presetElem.appendChild(sizeElem);
+    presetElem.appendChild(img);
+    presetElem.appendChild(canvas);
+    if (shortcut != null) {
+      const key = shortcut.shortcut.replace('MacCtrl', 'Ctrl');
+      const hotkeyInfo = document.createElement('span');
+      hotkeyInfo.className = 'hotkey';
+      hotkeyInfo.textContent = key;
+      presetElem.appendChild(hotkeyInfo);
     }
+    main.appendChild(presetElem);
+    drawRec(
+      canvas.id,
+      Math.round(preset.left / 100 * 45.6), Math.round(preset.top / 100 * 30),
+      Math.round(preset.width / 100 * 45.6), Math.round(preset.height / 100 * 30)
+    );
+  }
+  else {
+    const presetElem = document.createElement('div');
+    presetElem.className = 'preset';
+    presetElem.setAttribute('data-sortorder', preset.sortorder);
+    presetElem.setAttribute('data-id', preset.id);
+    main.appendChild(presetElem);
+  }
+}
 
-    function RemovePreset(dataID) {
-    data.map((obj) =>{
-      if(obj.id==dataID)
-      {
-        obj.isempty=true;
+function drawRec(canvasId, x, y, w, h) {
+  const canvas = document.getElementById(canvasId);
+  const ctx = canvas.getContext('2d');
+  ctx.fillStyle = '#663399';
+  ctx.rect(x + 9, y + 15, w, h);
+  ctx.fill();
+}
+
+
+let selectedElem;
+document.addEventListener('click', e => {
+  const dialog = document.getElementsByClassName('dialogWrapper')[0];
+  const dialogReset = document.getElementsByClassName('dialogWrapper')[1];
+
+  if (e.target.className == 'icon-trash') {
+    dialog.style.display = 'flex';
+    selectedElem = e.target.parentElement.getAttribute('data-id');
+  }
+  let presets;
+  const btnEdit = document.getElementById('btnEdit');
+  switch (e.target.id) {
+  case 'btnEdit':
+    presets = document.getElementsByClassName('preset');
+    if (e.target.state == undefined) {
+      for (let i = 0; i < presets.length; i++) {
+        presets[i].classList.add('editMode');
       }
-    });
-      chrome.storage.local.set({
-        "presets": data
-      }, () => {
-
-      });
+      e.target.classList.add('editMode-cancel');
+      e.target.state = 'IsInEditMode';
     }
-    resetPreset.addEventListener('click', () => {
-      let dialogForReset = document.getElementsByClassName("dialogWrapper")[1];
-        dialogForReset.style.display = "flex";
+    else {
+      for (let i = 0; i < presets.length; i++) {
+        presets[i].classList.remove('editMode');
+      }
+      e.target.classList.remove('editMode-cancel');
+      e.target.state = undefined;
+    }
+    break;
+  case 'btnNewPreset':
+    chrome.tabs.create({
+      'url': '../CustomPreset.html'
     });
+    break;
+  case 'btnCancel':
+    dialog.style.display = 'none';
+    break;
+  case 'btnResetCancel':
+    dialogReset.style.display = 'none';
+    break;
+  case 'btnOk':
+    dialog.style.display = 'none';
+    emptyMain();
+    removePreset(selectedElem);
+    presets = document.getElementsByClassName('preset');
+    for (let i = 0; i < presets.length; i++) {
+      presets[i].classList.remove('editMode');
+    }
+    if (btnEdit.state != undefined) {
+      btnEdit.classList.remove('editMode-cancel');
+      btnEdit.state = undefined;
+    }
+    data.sort(comparePresets);
+    for (let i = 0; i < data.length; i++) {
+      createPreset(data[i], i);
+    }
+    break;
+  case 'btnResetOk':
+    chrome.storage.local.remove('presets', () => chrome.runtime.sendMessage({
+      method: 'notify',
+      message: 'Layouts are reset to the default values'
+    }));
+    fetch('../Content/Data.json').then(r => r.json()).then(j => {
+      data = j.presets;
+      emptyMain();
+      initialize();
+    });
+    dialogReset.style.display = 'none';
+    break;
+  default:
+  }
+
+  if (e.target.className == 'preset') {
+    const x = parseInt(e.target.getAttribute('data-left'));
+    const y = parseInt(e.target.getAttribute('data-top'));
+    const w = parseInt(e.target.getAttribute('data-width'));
+    const h = parseInt(e.target.getAttribute('data-height'));
+    resizeWindow(x, y, w, h);
+  }
+});
+
+function emptyMain() {
+  while (main.firstChild) {
+    main.removeChild(main.firstChild);
+  }
+}
+
+function removePreset(dataID) {
+  data.map(obj =>{
+    if (obj.id === dataID) {
+      obj.isempty = true;
+    }
+  });
+  chrome.storage.local.set({
+    'presets': data
+  }, () => {
+
+  });
+}
+resetPreset.addEventListener('click', () => {
+  const dialogForReset = document.getElementsByClassName('dialogWrapper')[1];
+  dialogForReset.style.display = 'flex';
+});
--- Scripts/Resizer.js
+++ Scripts/Resizer.js
@@ -1,83 +1,82 @@
-/* eslint-disable */
+/* global workArea, data */
 
-'use strict'
-
-var data
+'use strict';
 
 function loadPresets(callback) {
-  chrome.storage.local.get(["presets"], results => {
+  chrome.storage.local.get(['presets'], results => {
     if (results.presets !== undefined) {
-     data = results.presets;
+      data = results.presets;
       data.sort(comparePresets);
-     callback(data)
-    } else {
-     let requestURL = '../Content/Data.json';
-     let request = new XMLHttpRequest();
-     request.open('GET', requestURL);
-     request.send();
-
-     request.onload = () => {
-      if (request.status === 200) {
-       data = JSON.parse(request.responseText).presets;
-       chrome.storage.local.set({
-        "presets": data
-       }, () => {
-        callback(data)
-       });
-      }
-     };
+      callback(data);
     }
-   });
+    else {
+      const requestURL = '../Content/Data.json';
+      const request = new XMLHttpRequest();
+      request.open('GET', requestURL);
+      request.send();
+
+      request.onload = () => {
+        if (request.status === 200) {
+          data = JSON.parse(request.responseText).presets;
+          chrome.storage.local.set({
+            'presets': data
+          }, () => {
+            callback(data);
+          });
+        }
+      };
+    }
+  });
 }
 
 // Firefox polyfill
 chrome.system = chrome.system || {
   display: {
     getInfo(flags, callback) {
-      const obj = {};
-      chrome.runtime.getBackgroundPage(bg => {
-        Object.assign(obj, {
-          top: bg.screen.top,
-          left: bg.screen.left,
-          width: bg.screen.width,
-          height: bg.screen.height
-        });
-        callback([{
-          bounds: obj,
-          workArea: obj
-        }]);
+      Promise.all([
+        new Promise(resolve => setTimeout(() => resolve(screen), 200)),
+        new Promise(resolve => chrome.runtime.getBackgroundPage(bg => {
+          resolve(bg.screen);
+        }))
+      ]).then(([a, b]) => {
+        callback([a, b].map(a => {
+          const obj = {};
+          Object.assign(obj, {
+            width: a.availWidth,
+            height: a.availHeight,
+            top: a.availTop,
+            left: a.availLeft
+          });
+          return {
+            bounds: obj,
+            workArea: obj
+          };
+        }));
       });
     }
   }
 };
 
 function resizeWindow(x, y, w, h) {
-  chrome.system.display.getInfo({}, info => {
-    chrome.windows.getCurrent(win => {
-      const o = info.filter(o => {
-        return win.left >= o.workArea.left &&
-          win.top >= o.workArea.top &&
-          win.width <= o.workArea.width &&
-          win.height <= o.workArea.height;
-      }).shift() || info[0];
-
-      chrome.windows.getCurrent(function(wind) {
-        var updateInfo = {
-          left: o.workArea.left + Math.round(x / 100 * o.workArea.width),
-          top: o.workArea.top + Math.round(y / 100 * o.workArea.height),
-          width: Math.round(w / 100 * o.workArea.width),
-          height: Math.round(h / 100 * o.workArea.height)
-        };
-        chrome.windows.update(wind.id, updateInfo, () => window.close());
-      });
-    });
+  chrome.windows.getCurrent(wind => {
+    if (w && h) {
+      const updateInfo = {
+        left: workArea.left + Math.round(x / 100 * workArea.width),
+        top: workArea.top + Math.round(y / 100 * workArea.height),
+        width: Math.round(w / 100 * workArea.width),
+        height: Math.round(h / 100 * workArea.height)
+      };
+      chrome.windows.update(wind.id, updateInfo, () => window.close());
+    }
   });
 }
 
 function comparePresets(a, b) {
-  if (parseInt(a.sortorder) > parseInt(b.sortorder))
+  if (parseInt(a.sortorder) > parseInt(b.sortorder)) {
     return 1;
-  if (parseInt(a.sortorder) < parseInt(b.sortorder))
+  }
+  if (parseInt(a.sortorder) < parseInt(b.sortorder)) {
     return -1;
+  }
   return 0;
 }
